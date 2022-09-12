e41e6c679f86301c3ad85746795630888c391c96
Ashawagandha-coder <max.cacz@yandex.ru>
2022-09-12 12:25:26 +0500

    add Seasons

--- src/main/kotlin/parse_code/Leagues/Seasons.kt
+++ src/main/kotlin/parse_code/Leagues/Seasons.kt
@@ -1,8 +1,51 @@
 package parse_code.Leagues
 
+import com.squareup.moshi.JsonClass
+import com.squareup.moshi.Moshi
+import parse_code.TimeZone.Paging
+
 fun main() {
 
+    val seasonsObject = Seasons().parse()
+
+    Seasons().action(seasonsObject)
+
+
+}
+
+class Seasons {
+
+
+    fun action(seasonsObject: SeasonsObject) {
+
+        val list_response: List<Int> = seasonsObject.response
+
+
+
+    }
+
+    fun parse(): SeasonsObject {
+
+        val moshi = Moshi.Builder().build()
+
+        val adapter = moshi.adapter<SeasonsObject>(SeasonsObject::class.java)
+
+        val seasonsObject = adapter.fromJson(seasons)
+
+        return seasonsObject!!
+
+    }
 
 
 
 }
+
+@JsonClass(generateAdapter = true)
+data class SeasonsObject(
+    val get: String,
+    val parameters: List<Any?>,
+    val errors: List<Any?>,
+    val results: Int,
+    val paging: Paging,
+    val response: List<Int>
+)
